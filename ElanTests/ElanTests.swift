fc38e0142e0bb523bee90436effb00e075b3f6d5
Snigdha <nganugap@in.ibm.com>
2015-04-25 14:47:28 +0530

    Initial Commit

---
+++ ElanTests/ElanTests.swift
@@ -0,0 +1,36 @@
+//
+//  ElanTests.swift
+//  ElanTests
+//
+//  Created by Snigdha Gayatri on 25/04/15.
+//  Copyright (c) 2015 Snigdha Gayatri. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ElanTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
